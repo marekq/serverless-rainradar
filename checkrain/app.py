1d892d3adbd46139d7ebf63720ed028e01190ef4
Marek Kuczynski <marek.kuczynski@gmail.com>
2021-01-27 23:58:30 +0100

    switched from buienradar to climacell.co api

--- checkrain/app.py
+++ checkrain/app.py
@@ -1,9 +1,8 @@
 from aws_lambda_powertools import Logger, Tracer
-import requests, os
-import matplotlib.pyplot as plt
+from datetime import datetime, timedelta
 
-from buienradar.buienradar import (get_data, parse_data)
-from buienradar.constants import (CONTENT, RAINCONTENT, SUCCESS)
+import json, requests, os, pytz
+import matplotlib.pyplot as plt
 
 modules_to_be_patched = ["boto3", "requests"]
 tracer = Tracer(patch_modules = modules_to_be_patched)
@@ -29,69 +28,79 @@ def get_weather():
 
     # set sendmsg to false
     sendmsg = False
+    msg = 'no rain'
 
     # set output message and rainchance to blank
-    msg = ''
-    rainchance = ''
+    timest = []
+    intensity = []
 
     # get lat and lon
     lat, lon = os.environ['latlon'].split(',')
     
-    # minutes to look ahead for precipitation forecast
-    timeframe = 60
+    url = "https://data.climacell.co/v4/timelines"
 
-    # get results
-    result = get_data(latitude = float(lat), longitude = float(lon))
+    # set time now and time plus three hours for forecase
+    start = datetime.now()
+    starttime = start.strftime('%Y-%m-%dT%H:%M:%SZ')
 
-    # if data was retrieved
-    if result.get(SUCCESS):
-        data = result[CONTENT]
-        raindata = result[RAINCONTENT]
+    end = datetime.now() + timedelta(hours = 2)
+    endtime = end.strftime('%Y-%m-%dT%H:%M:%SZ')
 
-        result = parse_data(data, raindata, float(lat), float(lon), timeframe)
+    # create query string for api
+    querystring = {
+        "location": os.environ['latlon'],
+        "fields": "precipitationIntensity",
+        "timesteps": "5m",
+        "units": "metric",
+        "startTime": starttime,
+        "endTime": endtime,
+        "apikey": os.environ['apikeyclimacell']
+    }
+
+    # request the forecase from climacell api
+    resp = requests.request("GET", url, params = querystring)
+    response = json.loads(resp.text)
+    print(response)
+
+    # go over interval responses
+    for item in response['data']['timelines'][0]['intervals']:
+    
+        # get time in Central European Timezone
+        utc_now = pytz.utc.localize(datetime.strptime(item['startTime'], '%Y-%m-%dT%H:%M:%SZ'))
+        cet_now = utc_now.astimezone(pytz.timezone("Europe/Amsterdam"))
+
+        # get precipation value
+        precipation = int(item['values']['precipitationIntensity'])
+
+        # add precipation and timestamp to lists
+        intensity.append(precipation)
+
+        # create valid, human readable timestamp (such as 11:34)
+        timest.append(str(cet_now.hour).zfill(2) + ":" + str(cet_now.minute).zfill(2))
         
-        rainchance = result['data']['forecast'][0]['rainchance']
-
-        if rainchance > 0:
+        # if rain is expected, set sendmessage to true
+        if precipation > 0:
             sendmsg = True
-
-            msg = result['data']['forecast'][0]['condition']['exact']
+            msg = 'rain expected'
 
     # if rain detected, send message
     if sendmsg:
         print('rain forecasted, sending message')
 
-        gen_graph(lat, lon)
-        send_message(msg, rainchance)
+        # generate line graph
+        gen_graph(timest, intensity)
+
+        # send the message to mobile devices
+        send_message(msg, "rain forcasted")
 
     else:
         print('no rain forecasted, quitting')
 
-def gen_graph(lat, lon):
+# generate line graph for push message
+@tracer.capture_method(capture_response = False)
+def gen_graph(timest, intensity):
 
-    times = []
-    rainmm = []
-
-    url = "https://gpsgadget.buienradar.nl/data/raintext?lat=" + lat + "&lon=" + lon
-    print(url)
-
-    res = requests.request("GET", url)
-    resp = str(res.text).split('|')
-
-    for line in resp:
-        if len(line) != 0:
-            item = line.split('\r\n')
-
-            if len(item) == int(2) and item[1] != '':
-                times.append(item[0])
-
-                if item[1] != '0':
-                    rainmm.append(item[1].lstrip('0'))
-
-                else:   
-                    rainmm.append(item[1])
-
-    plt.plot(times, rainmm)
+    plt.plot(timest, intensity)
     plt.xticks(rotation = 'vertical')
     plt.savefig('/tmp/plot.png')
 
